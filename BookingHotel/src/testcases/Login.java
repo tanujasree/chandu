8c7531b599574c78a76ab0bbe82b7fe1143b41ee
tanujasree <akhila.sree63@gmail.com>
2021-11-21 19:53:32 +0530

    self test

---
+++ BookingHotel/src/testcases/Login.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class Login 
+{
+  @Test
+  public void f() 
+  {
+	System.out.println("I am login test");
+ }
+  
+}
---
+++ BookingHotel/src/testcases/Runner.java
@@ -0,0 +1,38 @@
+package testcases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+
+public class Runner {
+
+	public static void main(String[] args) 
+	{
+		TestNG testng= new TestNG();
+		XmlSuite mysuite=new XmlSuite();
+		mysuite.setName("My suite");
+		
+		List<XmlSuite> allSuites=new ArrayList<XmlSuite>() ;
+         allSuites.add(mysuite);
+         testng.setXmlSuites(allSuites);
+         
+         XmlTest test= new XmlTest(mysuite);
+         test.setName("LoginTest");
+         
+        List<XmlClass> classes =new ArrayList<XmlClass>();
+        classes.add(new XmlClass("testcases.Login"));
+        test.setXmlClasses(classes);
+        testng.run();
+        
+
+        
+         
+         
+         
+	}
+
+}
